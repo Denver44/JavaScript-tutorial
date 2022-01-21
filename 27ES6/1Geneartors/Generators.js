b2e5e14a11f055b7b0f1178a9d5ff5047384218d
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-21 16:36:46 +0530

    Little fix

--- 27ES6/1Geneartors/Generators.js
+++ 27ES6/1Geneartors/Generators.js
@@ -1,7 +1,7 @@
 function* colors() {
-  yield "RED";
-  yield "GREEN";
-  yield "BLUE";
+  yield 'RED';
+  yield 'GREEN';
+  yield 'BLUE';
 }
 let color = colors();
 color.next();
@@ -21,10 +21,10 @@ colorsArr;
 
 const engineeringTeam = {
   size: 3,
-  department: "Engineering",
-  lead: "Jill",
-  manager: "Alex",
-  engineer: "Dave",
+  department: 'Engineering',
+  lead: 'Jill',
+  manager: 'Alex',
+  engineer: 'Dave',
 };
 
 function* TeamIterator(team) {
@@ -37,3 +37,24 @@ for (let members of TeamIterator(engineeringTeam)) {
   team.push(members);
 }
 team;
+
+console.log('this is tutorial 53');
+// Generators in JavaScript
+// 1 - 1B
+
+function* numbersGen() {
+  let i = 0;
+
+  // yield 1;
+  // yield 2;
+  // yield 3;
+  // yield 4;
+  while (true) {
+    yield i++;
+    // yield (i++).toString();
+  }
+}
+
+const gen = numbersGen();
+console.log(gen.next().value);
+console.log(gen.next().value);
--- 2JavaScript_Fundamentals_Part_2/3ArrowFuntion/script.js
+++ 2JavaScript_Fundamentals_Part_2/3ArrowFuntion/script.js
@@ -1,4 +1,49 @@
 // FAT ARROW FUNCTIONS OR ARROW FUNCTIONS
 
+// NORMAL FUNCTION
+const addTwo = function add(a, b) {
+  return a + b;
+};
+
+addTwo(5, 8);
+
+// FAT ARROW FUNCTIONS OR ARROW FUNCTIONS
+
 const add = (a, b) => a + b;
 add(5, 6);
+
+const numbers = [1, 2, 3];
+
+numbers.map((num) => num * 2);
+
+const fibonacci = (n) => {
+  if (n < 3) return 1;
+  else return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+fibonacci(5);
+
+const team = {
+  members: ['Jane', 'Bill'],
+  teamName: 'Super Squad',
+  teamSummary: function () {
+    return this.members.map(
+      function (member) {
+        return `${member} is on team ${this.teamName}`;
+      }.bind(this) // If we don't bind it will not get which this is its talking about
+    );
+  },
+};
+console.log(team.teamSummary());
+
+const team2 = {
+  members: ['Jane', 'Bill'],
+  teamName: 'Super Squad',
+  teamSummary: function () {
+    return this.members.map((member) => {
+      return `${member} is on team ${this.teamName}`;
+    });
+  },
+};
+
+console.log(team2.teamSummary());
